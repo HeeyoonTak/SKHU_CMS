69096d286dfee338a016ab985aed101550098d96
'Heeyoon <wewe1003@naver.com>
2020-04-06 01:29:17 +0900

    #1 소프픅젝 스프링 초기설정

---
+++ src/main/java/com/sofCap/SofCapApplication.java
@@ -0,0 +1,13 @@
+package com.sofCap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofCapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SofCapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofCap/SofCapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofCap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofCapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
