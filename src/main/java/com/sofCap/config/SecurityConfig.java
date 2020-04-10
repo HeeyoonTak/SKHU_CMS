487cc120015906e9a945ddca9152300eebcc7ff0
Heeyoon <wewe1003@naver.com>
2020-04-10 20:27:24 +0900

    #11 로그인 기본 설정

---
+++ src/main/java/com/sofCap/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.sofCap.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.sofCap.service.MyAuthenticationProvider;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired MyAuthenticationProvider myAuthenticationProvider;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http.authorizeRequests()
+			.antMatchers("/club_admin/**").hasRole("ClubAdmin")
+			.antMatchers("/club_union/**").hasRole("ClubUnion")
+			.antMatchers("/member/**").hasRole("Member")
+			.antMatchers("guest/**").permitAll()
+			.antMatchers("/").permitAll()
+			.antMatchers("/**").authenticated();
+
+		http.csrf().disable();
+
+		http.formLogin()
+			.loginPage("/guest/login")
+			.loginProcessingUrl("/guest/login_processing")
+			.failureUrl("/guest/login?error")
+			.defaultSuccessUrl("/")
+			.usernameParameter("login_id")
+			.passwordParameter("password");
+
+		http.logout()
+		.logoutRequestMatcher(new AntPathRequestMatcher("logout_processing"))
+		.logoutSuccessUrl("/guest/login")
+		.invalidateHttpSession(true);
+
+		http.authenticationProvider(myAuthenticationProvider);
+
+	}
+}
---
+++ src/main/java/com/sofCap/service/MyAuthenticationProvider.java
@@ -0,0 +1,81 @@
+package com.sofCap.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import com.sofCap.dto.UserDto;
+
+/*MyAuthenticationProvier 클래스는
+사용자가 입력한 로그인 아이디와 비밀번호를 검사할 때 사용되는 클래스이다.*/
+@Component
+public class MyAuthenticationProvider implements AuthenticationProvider {
+
+	@Autowired
+	UserServiceImpl userService;
+
+	/* 사용자가 입력한 로그인 아이디와 비밀번호가 authenticate 메소드의 파라미터로 전달된다. */
+	@Override
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		String loginId = authentication.getName();
+		String passwd = authentication.getCredentials().toString();
+		return authenticate(loginId, passwd);
+	}
+
+	public Authentication authenticate(String login_id, String password) throws AuthenticationException {
+		UserDto userDto = userService.login(login_id, password);
+		if (userDto == null) {
+			return null;
+		}
+		List<GrantedAuthority> grantedAuthorities = new ArrayList<GrantedAuthority>();
+		String role = "";
+		switch (userDto.getUser_type()) {
+		case "동연":
+			role = "ROLE_ClubUnion";
+			break;
+		case "동아리관리자":
+			role = "ROLE_ClubAdmin";
+			break;
+		case "일반회원":
+			role = "ROLE_Member";
+			break;
+		case "비회원":
+			role = "ROLE_guest";
+			break;
+		}
+
+		grantedAuthorities.add(new SimpleGrantedAuthority(role));
+		return new MyAuthenticaion(login_id, password, grantedAuthorities, userDto);
+	}
+	@Override
+	public boolean supports(Class<?> authentication) {
+		return authentication.equals(UsernamePasswordAuthenticationToken.class);
+	}
+
+	public class MyAuthenticaion extends UsernamePasswordAuthenticationToken {
+		private static final long serialVersionUID = 1L;
+		UserDto userDto;
+
+		public MyAuthenticaion(String loginId, String password, List<GrantedAuthority> grantedAuthorities, UserDto userDto) {
+			super(loginId, password, grantedAuthorities);
+			this.userDto = userDto;
+		}
+
+		public UserDto getUser() {
+			return userDto;
+		}
+
+		public void setUser(UserDto userDto) {
+			this.userDto = userDto;
+		}
+
+	}
+}
