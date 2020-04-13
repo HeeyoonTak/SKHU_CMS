ca3b4f326fef6172bbf4ae6950e106999bc64c5f
audtjr9514 <audtjr9514@gmail.com>
2020-04-13 20:16:39 +0900

    #14 동아리 관리 _ 목록, 삭제, 개설 완료

--- src/main/java/com/sofCap/controller/ClubManageController.java
+++ src/main/java/com/sofCap/controller/ClubManageController.java
@@ -15,44 +15,46 @@ import com.sofCap.mapper.UserMapper;
 @Controller
 @RequestMapping("/club_manage")
 public class ClubManageController {
-	@Autowired UserMapper userMapper;
-	
-	@RequestMapping("")
+	@Autowired
+	UserMapper userMapper;
+
+	@RequestMapping("list")
 	public String list(Model model) {
 		List<UserDto> users = userMapper.findAll();
-		model.addAttribute("users",users);;
+		model.addAttribute("users", users);
 		return "club_union/club_manage";
 	}
-	
-	@RequestMapping(value="create", method=RequestMethod.GET)
+
+	@RequestMapping(value = "create", method = RequestMethod.GET)
 	public String create(Model model) {
 		UserDto user = new UserDto();
-		model.addAttribute("user",user);
-		return "club_union/club_update";
+		model.addAttribute("user", user);
+		return "club_union/club_create";
 	}
-	
-	@RequestMapping(value="create", method=RequestMethod.POST)
+
+	@RequestMapping(value = "create", method = RequestMethod.POST)
 	public String create(Model model, UserDto user) {
 		userMapper.insert(user);
-		return "redirect:";
+		return "redirect:list";
 	}
-	
-	@RequestMapping(value="edit", method=RequestMethod.GET)
+
+	@RequestMapping(value = "edit", method = RequestMethod.GET)
 	public String edit(Model model, @RequestParam("id") int id) {
 		UserDto user = userMapper.findOne(id);
 		model.addAttribute("user", user);
 		return "club_union/club_update";
 	}
-    @RequestMapping(value="edit", method=RequestMethod.POST)
-    public String edit(Model model, UserDto user) {
-        userMapper.update(user);
-        return "redirect:";
-    }
 
-    @RequestMapping("delete")
-    public String delete(Model model, @RequestParam("id") int id) {
-        userMapper.delete(id);
-        return "redirect:";
-    }
+	@RequestMapping(value = "edit", method = RequestMethod.POST)
+	public String edit(Model model, UserDto user) {
+		userMapper.update(user);
+		return "redirect:list";
+	}
+
+	@RequestMapping("delete")
+	public String delete(Model model, @RequestParam("id") int id) {
+		userMapper.delete(id);
+		return "redirect:list";
+	}
 
 }
