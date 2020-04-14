ca9d6892a8ba334c47eca0874feefc53c653687d
nsap19 <zzz0484@naver.com>
2020-04-15 00:58:49 +0900

    #16 스키마 변경으로 인한 초기 데이터 설정 변경

--- 초기데이터 설정.sql
+++ 초기데이터 설정.sql
@@ -9,6 +9,9 @@ truncate table attendance;
 truncate table board;
 truncate table board_name;
 truncate table sem_date;
+truncate table account_files;
+truncate table board_files;
+truncate table club_files;
 SET FOREIGN_KEY_CHECKS = 1; -- Enable foreign key checking.
 
 ALTER TABLE `softwarecapstion`.`apply_a` 
@@ -35,27 +38,27 @@ INSERT INTO club_type (id,type_name) VALUES
   (4,'공연1'),
   (5,'공연2') ; 
   
-  INSERT INTO club (id,club_name, club_type, content, file_id) VALUES
-  (1,'동아리연합회', 1, null, null),
-  (2,'멋쟁이사자처럼', 2, null, null),
-  (3,'FLOW', 3, null, null),
-  (4,'ELPIS', 4, null, null),
-  (5,'아침햇살', 5, null, null),
-  (6,'All for RYU', 2, null, null),
-  (7,'행운', 5, null, null);
+  INSERT INTO club (id,club_name, club_type, content) VALUES
+  (1,'동아리연합회', 1, null),
+  (2,'멋쟁이사자처럼', 2, null),
+  (3,'FLOW', 3, null),
+  (4,'ELPIS', 4, null),
+  (5,'아침햇살', 5, null),
+  (6,'All for RYU', 2, null),
+  (7,'행운', 5, null);
 
- INSERT INTO account (id,club_id, price, total, remark, file_id, account_type, date) VALUES
-  (1,1, 200000, 200000, null, null, 0, now()),
-  (2,1, -200000, 0, null, null, 0, now()),
-  (3,1, 100000, 100000, null, null, 1, now()),
-  (4,1, -50000, 50000, null, null, 1, now()),
-  (5,2, 200000, 200000, null, null, 0, now()),
-  (6,2, -200000, 0, null, null, 0, now()),
-  (7,3, 200000, 200000, null, null, 0, now()),
-  (8,3, -200000, 0, null, null, 0, now()),
-  (9,4, 200000, 200000, null, null, 0, now()),
-  (10,4, -200000, 0, null, null, 0, now()),
-  (11,1, 200000, 250000, null, null, 0, '2020-10-10') ;
+ INSERT INTO account (id,club_id, price, total, remark, account_type, date) VALUES
+  (1,1, 200000, 200000, null,  0, now()),
+  (2,1, -200000, 0, null,  0, now()),
+  (3,1, 100000, 100000, null, 1, now()),
+  (4,1, -50000, 50000, null, 1, now()),
+  (5,2, 200000, 200000, null, 0, now()),
+  (6,2, -200000, 0, null, 0, now()),
+  (7,3, 200000, 200000, null, 0, now()),
+  (8,3, -200000, 0, null, 0, now()),
+  (9,4, 200000, 200000, null, 0, now()),
+  (10,4, -200000, 0, null, 0, now()),
+  (11,1, 200000, 250000, null, 0, '2020-10-10') ;
   
  INSERT INTO user (id, name, login_id, password, user_type, phone, email) VALUES
   (1, '동아리연합회', 'clubAs', '1234', '동연', null, null),
@@ -124,12 +127,12 @@ INSERT INTO board_name (id,board_name) VALUES
   (2, '모집'),
   (3, '공지') ;
   
- INSERT INTO board (id, board_name_id, title, content, date, file_id, club_id, start_date, end_date) VALUES
-  (1, 1, '아이디어톤 모집', '아이디어톤 홍보 공지', '2020-04-11', null, 2, null, null),
-  (2, 1, '해커톤 모집', '해커톤 홍보 공지', '2020-04-12', null, 2, null, null),
-  (3, 1, '공연 날짜 안내', '공연 날짜는 5/6부터 5/8까지 입니다.', '2020-04-12', null, 4, null, null),
-  (4, 2, '멋사 20기 모집', '20기 모집합니다.', '2020-04-10', null, 2, '2020-04-11', '2020-04-20'),
-  (5, 2, '아침햇살 모집', '아침햇살 모집합니다.', '2020-04-10', null, 5, '2020-04-12', '2020-04-21') ;
+ INSERT INTO board (id, board_name_id, title, content, date, club_id, start_date, end_date) VALUES
+  (1, 1, '아이디어톤 모집', '아이디어톤 홍보 공지', '2020-04-11', 2, null, null),
+  (2, 1, '해커톤 모집', '해커톤 홍보 공지', '2020-04-12', 2, null, null),
+  (3, 1, '공연 날짜 안내', '공연 날짜는 5/6부터 5/8까지 입니다.', '2020-04-12', 4, null, null),
+  (4, 2, '멋사 20기 모집', '20기 모집합니다.', '2020-04-10', 2, '2020-04-11', '2020-04-20'),
+  (5, 2, '아침햇살 모집', '아침햇살 모집합니다.', '2020-04-10', 5, '2020-04-12', '2020-04-21') ;
   
   INSERT INTO sem_date (id,start_date, end_date, sem_name) VALUES
   (1, '2020-03-01', '2020-08-31', '2020-1학기'),
