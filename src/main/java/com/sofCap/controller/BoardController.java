baa8fb295e70c14a7131a86f9835957c431178db
seyeonn <ansse17@gmail.com>
2020-04-21 15:21:59 +0900

    board dao/service 정리

--- src/main/java/com/sofCap/controller/BoardController.java
+++ src/main/java/com/sofCap/controller/BoardController.java
@@ -10,21 +10,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.sofCap.dto.BoardDto;
 import com.sofCap.dto.ClubDto;
-import com.sofCap.mapper.BoardMapper;
-import com.sofCap.mapper.ClubMapper;
+import com.sofCap.service.BoardService;
+import com.sofCap.service.ClubService;
 
 @Controller
 @RequestMapping("/")
 public class BoardController {
 	@Autowired
-	ClubMapper clubMapper;
+	ClubService clubService;
 	@Autowired
-	BoardMapper boardMapper;
+	BoardService boardService;
 
 	@RequestMapping("list-content")
 	public String list_content(Model model, @RequestParam("id") int club_id) {
-		ClubDto club = clubMapper.findById(club_id);
-		List<BoardDto> boards = boardMapper.findByClubId_p(club_id);
+		ClubDto club = clubService.findById(club_id);
+		List<BoardDto> boards = boardService.findByClubId_p(club_id);
 		model.addAttribute("club", club);
 		model.addAttribute("boards",boards);
 		return "guest/list-content";
@@ -32,63 +32,63 @@ public class BoardController {
 
 	@RequestMapping("c_p_content")
 	public String c_p_content(Model model, @RequestParam("id") int id) {
-		BoardDto board = boardMapper.findOne(id);
+		BoardDto board = boardService.findOne(id);
 		model.addAttribute("board",board);
 		return "guest/c_p_content";
 	}
 
 	@RequestMapping("p_content")
 	public String p_content(Model model, @RequestParam("id") int id) {
-		BoardDto board = boardMapper.findOne(id);
+		BoardDto board = boardService.findOne(id);
 		model.addAttribute("board",board);
 		return "guest/p_content";
 	}
 
 	@RequestMapping("r_content")
 	public String r_content(Model model, @RequestParam("id") int id) {
-		BoardDto board = boardMapper.findOne(id);
+		BoardDto board = boardService.findOne(id);
 		model.addAttribute("board",board);
 		return "guest/r_content";
 	}
 
 	@RequestMapping("n_content")
 	public String n_content(Model model, @RequestParam("id") int id) {
-		BoardDto board = boardMapper.findOne(id);
+		BoardDto board = boardService.findOne(id);
 		model.addAttribute("board",board);
 		return "club_union/n_content";
 	}
 
 	@RequestMapping("m_content")
 	public String m_content(Model model, @RequestParam("id") int id) {
-		BoardDto board = boardMapper.findOne(id);
+		BoardDto board = boardService.findOne(id);
 		model.addAttribute("board",board);
 		return "club_union/m_content";
 	}
 
 	@RequestMapping("publicity")
 	public String publicity(Model model) {
-		List<BoardDto> boards = boardMapper.findAll_p();
+		List<BoardDto> boards = boardService.findAll_p();
 		model.addAttribute("boards",boards);
 		return "guest/publicity";
 	}
 
 	@RequestMapping("recruit")
 	public String recruit(Model model) {
-		List<BoardDto> boards = boardMapper.findAll_r();
+		List<BoardDto> boards = boardService.findAll_r();
 		model.addAttribute("boards",boards);
 		return "guest/recruit";
 	}
 
 	@RequestMapping("notice")
 	public String union_notice(Model model) {
-		List<BoardDto> boards = boardMapper.findAll_n();
+		List<BoardDto> boards = boardService.findAll_n();
 		model.addAttribute("boards",boards);
 		return "club_union/union_notice";
 	}
 
 	@RequestMapping("minutes")
 	public String union_minutes(Model model) {
-		List<BoardDto> boards = boardMapper.findAll_m();
+		List<BoardDto> boards = boardService.findAll_m();
 		model.addAttribute("boards",boards);
 		return "club_union/union_minutes";
 	}
