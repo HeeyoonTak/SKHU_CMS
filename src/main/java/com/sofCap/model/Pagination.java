b5d5b903d04a0e99fa87a79d46ef8c50dda74140
nsap19 <zzz0484@naver.com>
2020-06-29 15:09:21 +0900

    Update Pagination.java

--- src/main/java/com/sofCap/model/Pagination.java
+++ src/main/java/com/sofCap/model/Pagination.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class Pagination {
 	int pg = 1;  // 현재 페이지
-    int sz = 5; // 페이지 당 레코드 수
+    int sz = 8; // 페이지 당 레코드 수
     int recordCount;
 
     public String getQueryString() {
